e6080d5504b6ba7a707d8037b7802c461f49af8e
khlee <khlee1290@gmail.com>
2015-12-02 11:47:03 +0900

    CC150 Cp1-1.1 ans

--- src/HashTableAns.java
+++ src/HashTableAns.java
@@ -8,7 +8,24 @@ public class HashTableAns{
 		
 		int checker=0;
 		for(int i=0;i<str.length();i++){
-			int val = str.charAt(i)
+			int val = str.charAt(i)-'a';
+			if((checker & (1<<val))>0){
+				return false;
+			}
+			checker |=(1<<val);
+		}
+		return true;
+	}
+	public boolean isUniqueChars2(String str){
+		if(str.length()>256) return false;
+		
+		boolean[] char_set = new boolean[256];
+		for(int i=0;i<str.length();i++){
+			int val = str.charAt(i);
+			if(char_set[val]){
+				return false;
+			}
+			char_set[val] = true;
 		}
 		return true;
 	}
